8a271a68dda08ba036446536baea72c2f814c281
Sergio Sanabria <sergioj.sanabria@gmail.com>
2015-12-28 11:10:48 -0300

    #1

--- src/AppBundle/Controller/NivelesRestController.php
+++ src/AppBundle/Controller/NivelesRestController.php
@@ -9,6 +9,9 @@ class NivelesRestController extends Controller {
 		$em      = $this->getDoctrine()->getManager();
 		$puertos = $em->getRepository( 'AppBundle:Puerto' )->findAll();
 
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 		return array( 'puertos' => $puertos );
 	}
 
