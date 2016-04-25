2f87ff838e5e963f20ea5d11e0c564cbc6c0ecb8
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2016-04-25 11:56:27 -0300

    Se crea servie para traer por puerto

--- src/AppBundle/Controller/NivelesRestController.php
+++ src/AppBundle/Controller/NivelesRestController.php
@@ -15,4 +15,17 @@ class NivelesRestController extends Controller {
 		return array( 'puertos' => $puertos );
 	}
 
+	public function getNivelesPuertoAction($puerto) {
+		$em      = $this->getDoctrine()->getManager();
+		$arrayCriteria = array('nombre'=> $puerto);
+
+		$puertos = $em->getRepository( 'AppBundle:Puerto' )->findOneBy($arrayCriteria);
+
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+		return $puertos;
+	}
+
+
 }
